3cb3f0ef67cb4874f663c0cc3f39be0cacaec90e
Chaos <antonioscutari2@hotmail.com>
2022-07-10 08:21:16 -0300

    [ADD] Cards rendering ok

--- client/src/components/Cards.jsx
+++ client/src/components/Cards.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { getDogs } from "../redux/actions";
 
 const Cards = () => {
-  return <div>Cards</div>;
-};
+  const dogs = useSelector((state) => state.allDogs);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getDogs());
+  }, [dispatch]);
 
+  if (dogs) {
+    return (
+      <div>
+        {dogs.map((d) => (
+          <div key={d.id}>
+            <h1>
+              <Link to={"/details/" + d.id}>{d.name}</Link>
+            </h1>
+            <p>{d?.temperament}</p>
+            <h2>✨: {d?.weight}</h2>
+            <img src={d?.image} preload="true" alt="cardimgerror" />
+          </div>
+        ))}
+      </div>
+    );
+  } else {
+    return <div>LOADER DEL GOBIERNO</div>;
+  }
+};
 export default Cards;
