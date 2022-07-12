ae8bfddf14a85ae60c909e5225637be3f9e11c0e
Chaos <antonioscutari2@hotmail.com>
2022-07-11 23:40:14 -0300

    [ADD] SerchVar

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { getQDogs } from "../redux/actions";
 
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  let dispatch = useDispatch();
+  let [name, setName] = useState("");
+  let handleInputChange = (e) => {
+    e.preventDefault();
+    setName(e.target.value);
+    dispatch(getQDogs(name));
+  };
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(getQDogs(name));
+  };
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Breed name..."
+        onChange={(e) => handleInputChange(e)}
+      />
+      <button type="submit" onClick={(e) => handleSubmit(e)}>
+        CALL
+      </button>
+    </div>
+  );
 };
 
 export default SearchBar;
