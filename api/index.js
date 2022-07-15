5ccbb10a7c2dfe570a99c94ac9db19f0d1de887d
Chaos <antonioscutari2@hotmail.com>
2022-07-14 23:01:32 -0300

    [ADD] sync false and calls allInfo when the server is up

--- api/index.js
+++ api/index.js
@@ -19,10 +19,11 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-
+const { allInfo } = require("./src/controllers/dogsController");
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(3001, () => {
+    allInfo();
     console.log("%s barking at 3001"); // eslint-disable-line no-console
   });
 });
