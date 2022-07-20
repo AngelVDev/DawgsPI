ce6ee83710d1a6431712e02979db9b87eaaccf67
Chaos <antonioscutari2@hotmail.com>
2022-07-20 05:32:15 -0300

    [STYLE] Landing done

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import "../styles/pages.css";
 const Landing = () => {
   return (
-    <div>
-      Landing
-      <button>
-        <Link to="/home">TO HOME</Link>
+    <div className="landingBg">
+      <div className="landingTextDiv">
+        <h1 style={{ fontWeight: "bold" }}>HENRY DOGGOS</h1>
+        <h3 style={{ fontWeight: "lighter" }}>
+          There's a lot of dogs waitin' for you behind the button below
+          <br />
+          ¡Come'n get yours!
+        </h3>
+      </div>
+      <button className="landingBtn">
+        <Link
+          style={{
+            textDecoration: "none",
+            color: "white",
+            fontFamily: "Cabin",
+          }}
+          to="/home"
+        >
+          ¡Let's go!
+        </Link>
       </button>
     </div>
   );
